faa9581224e92b8a902e3d57e9c1b0d691c82a67
seth127 <seth127@gmail.com>
2023-03-14 00:02:51 -0400

    fix: added dumbayz to stratgies/__init__.py

--- gamewinner/strategies/__init__.py
+++ gamewinner/strategies/__init__.py
@@ -1,6 +1,7 @@
 from gamewinner.strategies.evanmiya.mr_freeze import MrFreeze
 from gamewinner.strategies.evanmiya.slothfire_steady import SlothfireSteady  # noqa
 from gamewinner.strategies.evanmiya.the_cuts import TheCuts23  # noqa
+from gamewinner.strategies.evanmiya.the_cuts import TheCuts23DumBayz  # noqa
 from gamewinner.strategies.evanmiya.vanilla_miya import VanillaMiya  # noqa
 from gamewinner.strategies.examples.best_rank_wins import BestRankWins  # noqa
 from gamewinner.strategies.examples.worst_rank_wins import WorstRankWins  # noqa
@@ -11,6 +12,7 @@ available_strategies = (
     MrFreeze(),
     SlothfireSteady(),
     TheCuts23(),
+    TheCuts23DumBayz(),
     VanillaMiya(),
     WorstRankWins(),
 )
