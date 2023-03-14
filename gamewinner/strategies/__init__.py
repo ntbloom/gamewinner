3d9995c1597825e5d8a05ed29c5907efafb980af
seth127 <seth127@gmail.com>
2023-03-14 00:50:20 -0400

    feat: adding deterministic and bayz versions of SlothfireSteady and TheCuts

--- gamewinner/strategies/__init__.py
+++ gamewinner/strategies/__init__.py
@@ -1,7 +1,10 @@
 from gamewinner.strategies.evanmiya.mr_freeze import MrFreeze
+from gamewinner.strategies.evanmiya.slothfire_steady import SlothfireSteadiest  # noqa
 from gamewinner.strategies.evanmiya.slothfire_steady import SlothfireSteady  # noqa
+from gamewinner.strategies.evanmiya.slothfire_steady import SlothfireSteadyBayz  # noqa
 from gamewinner.strategies.evanmiya.the_cuts import TheCuts23  # noqa
 from gamewinner.strategies.evanmiya.the_cuts import TheCuts23DumBayz  # noqa
+from gamewinner.strategies.evanmiya.the_cuts import TheCuts23Frozen  # noqa
 from gamewinner.strategies.evanmiya.vanilla_miya import VanillaMiya  # noqa
 from gamewinner.strategies.examples.best_rank_wins import BestRankWins  # noqa
 from gamewinner.strategies.examples.worst_rank_wins import WorstRankWins  # noqa
@@ -11,7 +14,10 @@ available_strategies = (
     BestRankWins(),
     MrFreeze(),
     SlothfireSteady(),
+    SlothfireSteadiest(),
+    SlothfireSteadyBayz(),
     TheCuts23(),
+    TheCuts23Frozen(),
     TheCuts23DumBayz(),
     VanillaMiya(),
     WorstRankWins(),
--- gamewinner/strategies/evanmiya/the_cuts.py
+++ gamewinner/strategies/evanmiya/the_cuts.py
@@ -38,6 +38,36 @@ class TheCuts23(IEvanMiyaStrategy):
         return overall_score
 
 
+class TheCuts23Frozen(IEvanMiyaStrategy):
+    """
+    TheCuts23 without any randomness.
+
+    Final Four pick for 2023 below.
+
+
+    """
+
+    @property
+    def name(self) -> str:
+        return "TheCuts23"
+
+    def predict_score(self, winner: Team, loser: Team) -> tuple[int, int]:
+        return 82, 68
+
+    @no_type_check
+    def _team_metric(self, team: Team) -> float:
+        overall_score = (
+            self._rank_to_percentile(team.evanmiyaResumeRank) * team.evanmiyaBPR
+        )
+
+        # upset factor
+        overall_score = overall_score + (
+            0.5 * self._rank_to_percentile(team.evanmiyaHomeRank, reverse=True)
+        )
+
+        return overall_score
+
+
 class TheCuts23DumBayz(IEvanMiyaStrategy):
     """
     The Bayesian version of TheCuts23
