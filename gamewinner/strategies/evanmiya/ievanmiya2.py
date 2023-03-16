fb0e24deb8eea641846bcf0f66508d9f32718d89
Michael McDermott <104217788+michaelmcd18@users.noreply.github.com>
2023-03-15 22:56:20 -0400

    slight modification to allow 3 new columns

---
+++ gamewinner/strategies/evanmiya/ievanmiya2.py
@@ -0,0 +1,134 @@
+import csv
+from abc import ABC
+from pathlib import Path
+from random import randint
+from statistics import median
+from typing import Callable
+
+from gamewinner.strategies.istrategy import IStrategy
+from gamewinner.team import Team
+
+EVAN_MIYA_FILE = Path(__file__).parent.joinpath("data").joinpath("evanmiya-recent.csv")
+
+
+class IEvanMiyaStrategy2(IStrategy, ABC):
+    """
+    Class for strategies using Evan Miya data
+    The only thing you _have to_ implement is self._team_metric()
+    """
+
+    def prepare(self, teams: dict[str, Team]) -> None:
+        """
+        Loads all the Evan Miya data and attaches it to the teams
+        """
+        assert EVAN_MIYA_FILE.exists()
+
+        with open(EVAN_MIYA_FILE, "r") as f:
+            reader = csv.reader(f)
+            reader.__next__()
+            for row in reader:
+                (
+                    name,
+                    evanmiyaRank,
+                    evanmiyaOBPR,
+                    evanmiyaDBPR,
+                    evanmiyaBPR,
+                    evanmiyaOBPRChange,
+                    evanmiyaDBPRChange,
+                    evanmiyaBPRChange,
+                    evanmiyaOffRank,
+                    evanmiyaDefRank,
+                    evanmiyaKillShotsPerGame,
+                    evanmiyaKillShotsAllowedPerGame,
+                    evanmiyaTotalKillShots,
+                    evanmiyaTotalKillShotsAllowed,
+                    evanmiyaTrueTempo,
+                    evanmiyaTempoRank,
+                    evanmiyaInjuryRank,
+                    evanmiyaRosterRank,
+                    evanmiyaResumeRank,
+                    evanmiyaHomeRank,
+                ) = row
+
+                # add all Evan Miya stats to relevant team if they're in the tournament
+                if name in teams:
+                    team = teams[name]
+                    setattr(team, "evanmiyaRank", int(evanmiyaRank))
+                    setattr(team, "evanmiyaOBPR", float(evanmiyaOBPR))
+                    setattr(team, "evanmiyaDBPR", float(evanmiyaDBPR))
+                    setattr(team, "evanmiyaBPR", float(evanmiyaBPR))
+                    setattr(team, "evanmiyaOBPRChange", float(evanmiyaOBPRChange))
+                    setattr(team, "evanmiyaDBPRChange", float(evanmiyaDBPRChange))
+                    setattr(team, "evanmiyaBPRChange", float(evanmiyaBPRChange))
+                    setattr(team, "evanmiyaOffRank", int(evanmiyaOffRank))
+                    setattr(team, "evanmiyaDefRank", int(evanmiyaDefRank))
+                    setattr(team, "evanmiyaTrueTempo", float(evanmiyaTrueTempo))
+                    setattr(team, "evanmiyaTempoRank", int(evanmiyaTempoRank))
+                    setattr(team, "evanmiyaInjuryRank", int(evanmiyaInjuryRank))
+                    setattr(team, "evanmiyaRosterRank", int(evanmiyaRosterRank))
+                    setattr(
+                        team,
+                        "evanmiyaKillShotsPerGame",
+                        float(evanmiyaKillShotsPerGame),
+                    )
+                    setattr(
+                        team,
+                        "evanmiyaKillShotsAllowedPerGame",
+                        float(evanmiyaKillShotsAllowedPerGame),
+                    )
+                    setattr(team, "evanmiyaTotalKillShots", int(evanmiyaTotalKillShots))
+                    setattr(
+                        team,
+                        "evanmiyaTotalKillShotsAllowed",
+                        int(evanmiyaTotalKillShotsAllowed),
+                    )
+                    setattr(team, "evanmiyaResumeRank", int(evanmiyaResumeRank))
+                    setattr(team, "evanmiyaHomeRank", int(evanmiyaHomeRank))
+
+        for team in teams.values():
+            # make sure we found every team and added evanmiya attrs.
+            # could check all of them, but for now just check one.
+            assert hasattr(team, "evanmiyaRank"), f"skipped {team.name}"
+
+    def _team_metric(self, team: Team) -> float:
+        raise NotImplementedError
+
+    #########
+    # everything below here can be overriden, or you can inherit the defaults
+    #########
+
+    def predict_score(self, winner: Team, loser: Team) -> tuple[int, int]:
+        return randint(70, 78), randint(61, 69)
+
+    def pick(self, team1: Team, team2: Team) -> tuple[Team, Team]:
+        """
+        By default, this picks the team with the higher self._team_metric() score
+        """
+        team1_overall = self._team_metric(team1)
+        team2_overall = self._team_metric(team2)
+
+        self._log.debug(f"----- {team1} vs. {team2}")
+        self._log.debug(f"{team1}: {team1_overall}")
+        self._log.debug(f"{team2}: {team2_overall}")
+        if team1_overall > team2_overall:
+            self._log.debug(f"----- {team1} wins")
+            return team1, team2
+        else:
+            self._log.debug(f"----- {team2} wins")
+            return team2, team1
+
+    def _rank_to_percentile(self, rank: int, reverse: bool = False) -> float:
+        perc = max((251 - rank) / 250, 0)
+        if reverse:
+            perc = 1 - perc
+        return perc
+
+    def _dumbayz(self, func: Callable, numdraws: int = 1000) -> float:
+        """
+        A dumb Bayesian simulator.
+        Runs func() numdraws times and returns the median from all the runs
+        func -- a function that returns a float, i.e. a _team_metric() function)
+        numdraws -- number of times to run func(), defaults to 1000
+        """
+        ans = median([func() for x in range((numdraws - 1))])
+        return float(ans)
