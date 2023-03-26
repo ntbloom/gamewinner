eb519af42691c8aed31242f5dcbfe476909dcd73
seth127 <seth127@gmail.com>
2023-03-26 00:36:41 -0400

    TheOwl() takes flight. Florida Atlantic University heads to their first Final Four in program history.

--- gamewinner/strategies/__init__.py
+++ gamewinner/strategies/__init__.py
@@ -9,6 +9,7 @@ from gamewinner.strategies.evanmiya.slothfire_steady import SlothfireSteadyBayz 
 from gamewinner.strategies.evanmiya.the_cuts import TheCuts23  # noqa
 from gamewinner.strategies.evanmiya.the_cuts import TheCuts23DumBayz  # noqa
 from gamewinner.strategies.evanmiya.the_cuts import TheCuts23Frozen  # noqa
+from gamewinner.strategies.evanmiya.theowl import TheOwl  # noqa
 from gamewinner.strategies.evanmiya.the_white_whale import TheWhiteWhale
 from gamewinner.strategies.evanmiya.vanilla_miya import VanillaMiya  # noqa
 from gamewinner.strategies.examples.best_rank_wins import BestRankWins  # noqa
@@ -24,6 +25,7 @@ available_strategies = (
     TheCuts23(),
     TheCuts23Frozen(),
     TheCuts23DumBayz(),
+    TheOwl(),
     TheWhiteWhale(),
     Chillz(),
     KillerChillz(),
---
+++ gamewinner/strategies/evanmiya/theowl.py
@@ -0,0 +1,29 @@
+from typing import no_type_check
+
+from gamewinner.strategies.evanmiya.ievanmiya import IEvanMiyaStrategy
+from gamewinner.team import Team
+
+
+class TheOwl(IEvanMiyaStrategy):
+    """
+    SJB likes teams that play D and can get hot and go on a run.
+    Also, you gotta be able to play on the road.
+    """
+
+    @property
+    def name(self) -> str:
+        return "FireWaterFireWater"
+
+    @no_type_check
+    def _team_metric(self, team: Team) -> float:
+        overall_score = (
+            self._rank_to_percentile(team.evanmiyaDefRank)
+            - 0.75 * team.evanmiyaKillShotsAllowedPerGame
+            + 0.75 * team.evanmiyaKillShotsPerGame
+            + 0.75 * self._rank_to_percentile(team.evanmiyaInjuryRank)
+            + 0.75 * self._rank_to_percentile(team.evanmiyaHomeRank, reverse=True)
+        )
+        return overall_score
+
+    def predict_score(self, winner: Team, loser: Team) -> tuple[int, int]:
+        return 76, 70
