874d70b5dd6b78a7dcaae82e4caf53ac60739592
Seth Green <seth127@gmail.com>
2023-03-27 21:12:38 -0400

    TheOwl() takes flight (#20)
    * TheOwl() takes flight. Florida Atlantic University heads to their first Final Four in program history

--- gamewinner/strategies/__init__.py
+++ gamewinner/strategies/__init__.py
@@ -1,22 +1,30 @@
-from gamewinner.strategies.evanmiya.chillz import Chillz  # noqa
-from gamewinner.strategies.evanmiya.chillz import KillerChillz  # noqa
-from gamewinner.strategies.evanmiya.doctor_lizard import DoctorLizard  # noqa
-from gamewinner.strategies.evanmiya.firewater import FireWaterFireWater  # noqa
-from gamewinner.strategies.evanmiya.mr_freeze import MrFreeze  # noqa
-from gamewinner.strategies.evanmiya.slothfire_steady import SlothfireSteadiest  # noqa
-from gamewinner.strategies.evanmiya.slothfire_steady import SlothfireSteady  # noqa
-from gamewinner.strategies.evanmiya.slothfire_steady import SlothfireSteadyBayz  # noqa
-from gamewinner.strategies.evanmiya.the_cuts import TheCuts23  # noqa
-from gamewinner.strategies.evanmiya.the_cuts import TheCuts23DumBayz  # noqa
-from gamewinner.strategies.evanmiya.the_cuts import TheCuts23Frozen  # noqa
-from gamewinner.strategies.evanmiya.the_white_whale import TheWhiteWhale  # noqa
-from gamewinner.strategies.evanmiya.vanilla_miya import VanillaMiya  # noqa
-from gamewinner.strategies.examples.best_rank_wins import BestRankWins  # noqa
-from gamewinner.strategies.examples.worst_rank_wins import WorstRankWins  # noqa
+from gamewinner.strategies.evanmiya.chillz import Chillz, KillerChillz
+from gamewinner.strategies.evanmiya.doctor_lizard import DoctorLizard
+from gamewinner.strategies.evanmiya.firewater import FireWaterFireWater
+from gamewinner.strategies.evanmiya.mr_freeze import MrFreeze
+from gamewinner.strategies.evanmiya.slothfire_steady import (
+    SlothfireSteadiest,
+    SlothfireSteady,
+    SlothfireSteadyBayz,
+)
+from gamewinner.strategies.evanmiya.the_cuts import (
+    TheCuts23,
+    TheCuts23DumBayz,
+    TheCuts23Frozen,
+)
+from gamewinner.strategies.evanmiya.the_white_whale import TheWhiteWhale
+from gamewinner.strategies.evanmiya.theowl import TheOwl
+from gamewinner.strategies.evanmiya.vanilla_miya import VanillaMiya
+from gamewinner.strategies.examples.best_rank_wins import BestRankWins
+from gamewinner.strategies.examples.worst_rank_wins import WorstRankWins
 from gamewinner.strategies.istrategy import Strategy  # noqa
 
 available_strategies = (
     BestRankWins(),
+    Chillz(),
+    DoctorLizard(),
+    FireWaterFireWater(),
+    KillerChillz(),
     MrFreeze(),
     SlothfireSteady(),
     SlothfireSteadiest(),
@@ -24,11 +32,8 @@ available_strategies = (
     TheCuts23(),
     TheCuts23Frozen(),
     TheCuts23DumBayz(),
+    TheOwl(),
     TheWhiteWhale(),
-    Chillz(),
-    KillerChillz(),
     VanillaMiya(),
-    DoctorLizard(),
-    FireWaterFireWater(),
     WorstRankWins(),
 )
---
+++ gamewinner/strategies/evanmiya/theowl.py
@@ -0,0 +1,29 @@
+from gamewinner.strategies.evanmiya.ievanmiya import IEvanMiyaStrategy
+from gamewinner.team import Team
+
+
+class TheOwl(IEvanMiyaStrategy):
+    """
+    SJB likes teams that play D and can get hot and go on a run.
+    Also, you gotta be able to play on the road.
+
+    For the record, this is the original: https://github.com/ntbloom/gamewinner/pull/20/
+    """
+
+    @property
+    def name(self) -> str:
+        return "TheOwl"
+
+    def _team_metric(self, team: Team) -> float:
+        props = self.get_props(team)
+        overall_score = (
+            self._rank_to_percentile(props.def_rank)
+            - 0.75 * props.kill_shots_allowed_per_game
+            + 0.75 * props.kill_shots_per_game
+            + 0.75 * self._rank_to_percentile(props.injury_rank)
+            + 0.75 * self._rank_to_percentile(props.home_rank, reverse=True)
+        )
+        return overall_score
+
+    def predict_score(self, winner: Team, loser: Team) -> tuple[int, int]:
+        return 76, 70
