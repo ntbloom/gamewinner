4d4b9af591193e72136d27a684717d4dbebe2489
seth127 <seth127@gmail.com>
2024-03-21 09:04:25 -0400

    2024 updates to the_cuts.py. Switch resume_rank out for rank_roster, same as 35828248449d32a2461b23810bfae19865ee333a

--- gamewinner/strategies/__init__.py
+++ gamewinner/strategies/__init__.py
@@ -5,12 +5,19 @@ from gamewinner.strategies.mathstats.derived.chillz import Chillz, KillerChillz
 from gamewinner.strategies.mathstats.derived.doctor_lizard import DoctorLizard
 from gamewinner.strategies.mathstats.derived.firewater import FireWaterFireWater
 from gamewinner.strategies.mathstats.derived.mr_freeze import MrFreeze
+from gamewinner.strategies.mathstats.derived.slothfire_steady import (
+    SlothfireSteadiest,
+    SlothfireSteady,
+    SlothfireSteadyBayz,
+)
+from gamewinner.strategies.mathstats.derived.the_cuts import (
+    TheCuts23,
+    TheCuts23DumBayz,
+    TheCuts23Frozen,
+)
 from gamewinner.strategies.mathstats.derived.the_white_whale import TheWhiteWhale
 from gamewinner.strategies.mathstats.derived.theowl import TheOwl
 from gamewinner.strategies.mathstats.derived.vanilla import Vanilla
-from gamewinner.strategies.mathstats.derived.slothfire_steady import SlothfireSteady
-from gamewinner.strategies.mathstats.derived.slothfire_steady import SlothfireSteadiest
-from gamewinner.strategies.mathstats.derived.slothfire_steady import SlothfireSteadyBayz
 
 available_strategies = (
     BestRankWins(),
@@ -26,4 +33,7 @@ available_strategies = (
     SlothfireSteady(),
     SlothfireSteadiest(),
     SlothfireSteadyBayz(),
+    TheCuts23(),
+    TheCuts23Frozen(),
+    TheCuts23DumBayz(),
 )
