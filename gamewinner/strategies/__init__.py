10572467c8a892a3d76963d0567e858138558e39
Seth Green <seth127@gmail.com>
2024-03-26 20:55:02 -0400

    2024 updates to defunct strategies (#28)
    * 2024 slothfire_steady.py initial edits. Just replaced resume_rank with rank_roster in all three strategies and added them all to strategies/__init__.py. Not a great replacement, but it seems to be the best on a short time frame. Thanks to @ntbloom for the updates the tips on getting in here.
    
    * removed strategies/evanmiya/ and strategies/espn/ that accidentally got committed and push on the previous commit. Those are old and not working (wont even format correctly with black).
    
    * 2024 updates to the_cuts.py. Switch resume_rank out for rank_roster, same as 35828248449d32a2461b23810bfae19865ee333a

--- gamewinner/strategies/__init__.py
+++ gamewinner/strategies/__init__.py
@@ -5,6 +5,16 @@ from gamewinner.strategies.mathstats.derived.chillz import Chillz, KillerChillz
 from gamewinner.strategies.mathstats.derived.doctor_lizard import DoctorLizard
 from gamewinner.strategies.mathstats.derived.firewater import FireWaterFireWater
 from gamewinner.strategies.mathstats.derived.mr_freeze import MrFreeze
+from gamewinner.strategies.mathstats.derived.slothfire_steady import (
+    SlothfireSteadiest,
+    SlothfireSteady,
+    SlothfireSteadyBayz,
+)
+from gamewinner.strategies.mathstats.derived.the_cuts import (
+    TheCuts23,
+    TheCuts23DumBayz,
+    TheCuts23Frozen,
+)
 from gamewinner.strategies.mathstats.derived.the_white_whale import TheWhiteWhale
 from gamewinner.strategies.mathstats.derived.theowl import TheOwl
 from gamewinner.strategies.mathstats.derived.vanilla import Vanilla
@@ -20,4 +30,10 @@ available_strategies = (
     TheWhiteWhale(),
     Vanilla(),
     WorstRankWins(),
+    SlothfireSteady(),
+    SlothfireSteadiest(),
+    SlothfireSteadyBayz(),
+    TheCuts23(),
+    TheCuts23Frozen(),
+    TheCuts23DumBayz(),
 )
