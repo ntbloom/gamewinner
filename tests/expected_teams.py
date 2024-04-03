3013cde8f77c92bfff4010e1b07394b65e88f9ff
ntbloom <ntbloom@gmail.com>
2024-04-02 23:17:48 -0400

    refactored test data

--- tests/expected_teams.py
+++ tests/expected_teams.py
@@ -1,8 +1,52 @@
 from gamewinner.teams.team import get_definitive_name
 
-FIRST_ROUND2024 = [
-    {get_definitive_name(game.pop()), get_definitive_name(game.pop())}
-    for game in [
+matchup = set[str]
+
+
+def make_set(matchups: list[matchup]) -> list[matchup]:
+    return [
+        {get_definitive_name(game.pop()), get_definitive_name(game.pop())}
+        for game in matchups
+    ]
+
+
+class ExpectedTeamData:
+    def __init__(
+        self,
+        winner: str,
+        finals: matchup,
+        final_four: list[matchup],
+        elite_eight: list[matchup],
+        first_round: list[matchup],
+    ):
+        self.winner = get_definitive_name(winner)
+        self.finals = {get_definitive_name(game) for game in finals}
+        self.final_four = make_set(final_four)
+        self.elite_eight = make_set(elite_eight)
+
+        self.first_round = make_set(first_round)
+
+        assert self.winner
+        assert len(final_four) == 2
+        assert len(elite_eight) == 4
+
+        assert len(first_round) == 32
+
+
+Expected2024 = ExpectedTeamData(
+    winner="UConn",
+    finals={"Houston", "UConn"},
+    final_four=[
+        {"UNC", "UConn"},
+        {"Houston", "Purdue"},
+    ],
+    elite_eight=[
+        {"UConn", "Iowa State"},
+        {"UNC", "Arizona"},
+        {"Purdue", "Tennessee"},
+        {"Houston", "Marquette"},
+    ],
+    first_round=[
         {"UNC", "Wagner"},
         {"Mississippi State", "Michigan State"},
         {"Saint Mary's", "Grand Canyon"},
@@ -35,24 +79,5 @@ FIRST_ROUND2024 = [
         {"Kentucky", "Oakland"},
         {"Florida", "Colorado"},
         {"Marquette", "Western Kentucky"},
-    ]
-]
-ELITE_EIGHT2024 = [
-    {get_definitive_name(game.pop()), get_definitive_name(game.pop())}
-    for game in [
-        {"UConn", "Iowa State"},
-        {"UNC", "Arizona"},
-        {"Purdue", "Tennessee"},
-        {"Houston", "Marquette"},
-    ]
-]
-
-FINAL_FOUR2024 = [
-    {get_definitive_name(game.pop()), get_definitive_name(game.pop())}
-    for game in [
-        {"UNC", "UConn"},
-        {"Houston", "Purdue"},
-    ]
-]
-
-FINALS2024 = {get_definitive_name(game) for game in ("Houston", "UConn")}
+    ],
+)
--- tests/test_bracket.py
+++ tests/test_bracket.py
@@ -1,10 +1,9 @@
 import pytest
 
-import tests.expected_teams as expected_teams
 from gamewinner.bracket.bracket import Bracket
-from gamewinner.bracket.game import Game
 from gamewinner.bracket.parser import Parser
-from gamewinner.teams.team import Team, get_definitive_name
+from gamewinner.teams.team import Team
+from tests.expected_teams import Expected2024, ExpectedTeamData
 
 
 class TestBasicBuild:
@@ -20,75 +19,44 @@ class TestBasicBuild:
         for team in parser.teams:
             assert isinstance(team, Team)
 
-    def test_bracket_builds(self, test_year: int) -> None:
+    def test_bracket_builds_and_plays(self, test_year: int) -> None:
         bracket = Bracket(test_year)
         assert bracket
         assert len(set(bracket.teams)) == 64
 
-
-class TestBracketPlayBestWins:
-    @pytest.mark.parametrize(
-        "year,first_round_games",
-        [
-            (2024, expected_teams.FIRST_ROUND2024),
-        ],
-    )
-    def test_first_round_games(self, year: int, first_round_games: set[Game]) -> None:
-        bracket = Bracket(year)
         bracket.play()
         assert len(bracket.games) == 63
-        assert len(first_round_games) == 32
-        assert len(bracket.first_round) == 32
-        for game in bracket.first_round:
-            teams = {game.team1.name, game.team2.name}
-            assert teams in first_round_games
-
-    @pytest.mark.parametrize(
-        "year,elite_eight",
-        [
-            (2024, expected_teams.ELITE_EIGHT2024),
-        ],
-    )
-    def test_elite_eight(self, year: int, elite_eight: set[set[str]]) -> None:
-        bracket = Bracket(year)
-        bracket.play()
-        assert len(bracket.elite_eight) == 4
-        for game in bracket.elite_eight:
-            teams = {game.team1.name, game.team2.name}
-            assert teams in elite_eight
-
-    @pytest.mark.parametrize(
-        "year,final_four",
-        [
-            (2024, expected_teams.FINAL_FOUR2024),
-        ],
-    )
-    def test_final_four(self, year: int, final_four: set[set[str]]) -> None:
-        bracket = Bracket(year)
-        bracket.play()
+        assert bracket.winner
         assert len(bracket.final_four) == 2
+        assert len(bracket.elite_eight) == 4
+        # assert len(bracket.sweet_sixteen) == 8
+        # assert len(bracket.second_round) == 16
+        assert len(bracket.first_round) == 32
+
+
+@pytest.mark.parametrize("year,expected_data", [(2024, Expected2024)])
+class TestBracketPlayBestWins:
+    def test_first_round_games(
+        self, year: int, expected_data: ExpectedTeamData
+    ) -> None:
+        bracket = Bracket(year)
+        bracket.play()
+
+        assert bracket.winner.name == expected_data.winner
+
+        assert {
+            bracket.finals.team1.name,
+            bracket.finals.team2.name,
+        } == expected_data.finals
+
         for game in bracket.final_four:
             teams = {game.team1.name, game.team2.name}
-            assert teams in final_four
+            assert teams in expected_data.final_four
 
-    @pytest.mark.parametrize(
-        "year,finals",
-        [
-            (2024, expected_teams.FINALS2024),
-        ],
-    )
-    def test_finals(self, year: int, finals: set[str]) -> None:
-        bracket = Bracket(year)
-        bracket.play()
-        assert {bracket.finals.team1.name, bracket.finals.team2.name} == finals
+        for game in bracket.elite_eight:
+            teams = {game.team1.name, game.team2.name}
+            assert teams in expected_data.elite_eight
 
-    @pytest.mark.parametrize(
-        "year,predicted_winner",
-        [
-            (2024, "UConn"),
-        ],
-    )
-    def test_winner(self, year: int, predicted_winner: str) -> None:
-        bracket = Bracket(year)
-        bracket.play()
-        assert bracket.winner.name == get_definitive_name(predicted_winner)
+        for game in bracket.first_round:
+            teams = {game.team1.name, game.team2.name}
+            assert teams in expected_data.first_round
